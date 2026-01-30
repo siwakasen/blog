044ff5dedf0f1904b4fc4b1de6b9f81ac9b9a542
siwakasen <siwakasen@gmail.com>
2026-01-30 19:45:12 +0800

    feat: adding techstack

---
+++ src/components/Typography/ExternalLogoLink.tsx
@@ -0,0 +1,66 @@
+import * as Tooltip from '@radix-ui/react-tooltip';
+
+import { cn } from '@/utils/styles/classNames';
+
+import { LinkPreview } from '../LinkPreview';
+
+import { useLogoClassName } from './hooks/useAnchorClassName';
+
+interface Props extends React.HTMLProps<HTMLAnchorElement> {
+  href: string;
+  className?: string;
+  onClick?: (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => void;
+  children?: React.ReactNode;
+  shouldShowPreviewOnHover?: boolean;
+
+  isNotFancy?: boolean; // Disables the box-shadow highlight effect on hover/focus
+}
+
+export const ExternalLogoLink: React.FC<Props> = ({
+  href,
+  className,
+  onClick,
+  children,
+  shouldShowPreviewOnHover = false,
+  isNotFancy = false,
+}) => {
+  const baseClass = useLogoClassName();
+
+  if (!shouldShowPreviewOnHover) {
+    return (
+      <a
+        className={cn('not-prose', { [baseClass]: !isNotFancy }, className)}
+        href={href}
+        onClick={onClick}
+        target="_blank"
+        rel="noreferrer noopener"
+      >
+        {children}
+      </a>
+    );
+  }
+
+  return (
+    <Tooltip.Provider delayDuration={300} skipDelayDuration={200}>
+      <Tooltip.Root>
+        <Tooltip.Trigger asChild>
+          <a
+            className={cn({ [baseClass]: !isNotFancy }, className)}
+            href={href}
+            onClick={onClick}
+            target="_blank"
+            rel="noreferrer"
+          >
+            {children}
+          </a>
+        </Tooltip.Trigger>
+        <Tooltip.Portal>
+          <Tooltip.Content>
+            <LinkPreview href={href} />
+          </Tooltip.Content>
+        </Tooltip.Portal>
+      </Tooltip.Root>
+    </Tooltip.Provider>
+  );
+};
+
