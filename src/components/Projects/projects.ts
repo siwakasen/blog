fcfbc4153b6cccd764fedbb8c7a88d0d69661ee2
siwakasen <siwakasen@gmail.com>
2026-04-24 16:06:17 +0700

    fix: remove deprecated projects

--- src/components/Projects/projects.ts
+++ src/components/Projects/projects.ts
@@ -12,18 +12,4 @@ export const projects: Project[] = [
     repo: 'https://github.com/stars/siwakasen/lists/how-to-pronounce-in-english',
     coverImage: 'https://say.siwakasen.dev/un.png',
   },
-  {
-    name: 'Bali Travel Ride',
-    url: 'https://travel.vulpbox.com',
-    repo: 'https://github.com/siwakasen/tourism-microservices',
-    coverImage:
-      'https://travel.vulpbox.com/_next/image?url=%2Fimages%2Fhero4_img.jpg&w=1920&q=75',
-  },
-  {
-    name: 'Tekik Village',
-    url: 'https://tekik.vercel.app',
-    repo: 'https://github.com/siwakasen/tekik',
-    coverImage:
-      'https://tekik.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fgambar.516c6652.jpg&w=3840&q=75',
-  },
 ];
